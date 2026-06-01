acfd611b8216047ff4e8a75493a4c34d0cc16d0e
Sonu Kumar Paswan <sonupaswan0381@gmail.com>
2026-06-01 12:45:33 +0530

    add redis

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,17 +1,7 @@
-import { createClient } from "redis";
+const redis=require('redis');
 
-const redisClient = createClient({
-  url: "redis://localhost:6379",
+const client=redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD
 });
-
-redisClient.on("connect", () => {
-  console.log(" Redis Connected");
-});
-
-redisClient.on("error", (err) => {
-  console.log(" Redis Error:", err);
-});
-
-await redisClient.connect();
-
-export default redisClient;
