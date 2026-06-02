59a27a60299a485b45711b819d2b32990dfdcce3
Sonu Kumar Paswan <sonupaswan0381@gmail.com>
2026-06-02 15:35:20 +0530

    update main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import Commercial from "@/components/home/Commercial";
 import Hero from "@/components/home/Hero";
+import ServicesProvider from "@/components/home/ServicesProvider";
 import Navbar from "@/components/layout/Navbar";
 
 export default function Home() {
@@ -8,6 +10,8 @@ export default function Home() {
     <div>
       <Navbar />
       <Hero />
+        <Commercial />
+       <ServicesProvider />
     </div>
   );
 }
