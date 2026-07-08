3373087657b9c439dbfb16dad39ea909f7d18026
Sonu  Paswan <120728933+SonuKumarPaswan@users.noreply.github.com>
2026-07-08 18:37:55 +0530

    Update API base URL to mishti-backend-2

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
     const api = axios.create({
-        baseURL: "https://mishti-backend-3.onrender.com/api",
+     //   baseURL: "https://mishti-backend-3.onrender.com/api",
+        baseURL: "https://mishti-backend-2.onrender.com/api",
         // baseURL: "http://localhost:8085/api", 
     }); 
 
